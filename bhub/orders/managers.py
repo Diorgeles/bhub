9f47f9ad5194ef2aa3920e6ed1d89138e53ec2e6
diorgelesdias <diorgelesdias@gmail.com>
2023-12-25 22:20:43 -0300

    fix: fixed return manager order and payment

--- bhub/orders/managers.py
+++ bhub/orders/managers.py
@@ -1,47 +1,24 @@
-from abc import ABCMeta, abstractmethod
-
 from django.db.models import Manager
 
-from bhub.associates.services import Associate
-from bhub.notifications.services import Notification
-from bhub.payments.services import PaymentHelpers
-from bhub.products.services import Product
-from bhub.shipments.services import Shipping
+from bhub.orders.rules import (
+    Book,
+    MemberAssociation,
+    PhysicalProduct,
+    UpgradeMemberAssociation,
+    Video,
+)
 
 
-class PostPaymentOrderManager(Manager, Notification, Associate, Product):
+class PostPaymentOrderManager(Manager):
     def execute(self, order):
         match order.order_type:
             case "physical_product":
-                print()
-                # PhysicalProduct().execute(order)
+                return PhysicalProduct().execute(order)
             case "book":
-                Book().execute(order)
+                return Book().execute(order)
             case "member_association":
-                self.handle_active_member_association(order)
-                self.send_email(order)
+                return MemberAssociation().execute(order)
             case "upgrade_member_association":
-                self.handle_upgrade_member_association(order)
-                self.send_email(order)
+                return UpgradeMemberAssociation().execute(order)
             case "video":
-                self.handle_add_video(order)
-
-
-class OrderType(metaclass=ABCMeta):
-    @abstractmethod
-    def execute(self, order):
-        raise NotImplementedError()
-
-
-class PhysicalProduct(OrderType, PaymentHelpers, Shipping):
-    def create(self):
-        pass
-
-    # def execute(self, order):
-    #     self.handle_shipment_for_shipping(order)
-    #     self.pay_commision(order)
-
-
-class Book(Shipping):
-    def execute(self, order):
-        self.handle_duplicate_shipmentf_for_royalties(order)
+                return Video().execute(order)
--- bhub/payments/managers.py
+++ bhub/payments/managers.py
@@ -8,5 +8,4 @@ class PaymentManager(Manager, PaymentHelpers):
     def pay(self, order):
         print("Pagamento salvo localmente...")
         print("Executa pagamento...")
-        Order.objects_post_payment.execute(order)
-        return dict(detail=f"Pedido {order.id} pago com sucesso", status=200)
+        return Order.objects_post_payment.execute(order)
