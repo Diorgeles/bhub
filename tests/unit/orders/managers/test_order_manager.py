c5fa0efbe2f0466f92248e80a1f15f6d648d1cea
diorgelesdias <diorgelesdias@gmail.com>
2023-12-25 22:45:09 -0300

    test: add assert to validate create payment to order

--- tests/unit/orders/managers/test_order_manager.py
+++ tests/unit/orders/managers/test_order_manager.py
@@ -7,7 +7,36 @@ from bhub.payments.models import Payment
 @mark.only
 class TestUnitOrderManager:
     @mark.django_db
-    def test_pay_order(self):
+    def test_pay_order_type_physical_product(self):
         order = Order.objects.create(name="Teste1", amount=10, order_type="physical_product")
-        Payment.objects.pay(order)
-        print(order.order_type)
+        pay = Payment.objects.pay(order)
+        assert order is not None
+        assert pay is not None
+
+    @mark.django_db
+    def test_pay_order_type_book(self):
+        order = Order.objects.create(name="Teste1", amount=10, order_type="book")
+        pay = Payment.objects.pay(order)
+        assert order is not None
+        assert pay is not None
+
+    @mark.django_db
+    def test_pay_order_type_member_association(self):
+        order = Order.objects.create(name="Teste1", amount=10, order_type="member_association")
+        pay = Payment.objects.pay(order)
+        assert order is not None
+        assert pay is not None
+
+    @mark.django_db
+    def test_pay_order_type_upgrade_member_association(self):
+        order = Order.objects.create(name="Teste1", amount=10, order_type="upgrade_member_association")
+        pay = Payment.objects.pay(order)
+        assert order is not None
+        assert pay is not None
+
+    @mark.django_db
+    def test_pay_order_type_video(self):
+        order = Order.objects.create(name="Teste1", amount=10, order_type="video")
+        pay = Payment.objects.pay(order)
+        assert order is not None
+        assert pay is not None
